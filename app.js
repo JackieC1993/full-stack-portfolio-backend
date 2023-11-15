7fe41799bb30ce1a86e5c8177cea4b602934b0d9
JackieC1993 <jackiecheung@pursuit.org>
2023-11-15 00:57:15 -0500

    fixed the route to middleware

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const dimSumsControllers = require('./controllers/dimSumController')
+const morgan = require('morgan');
+const dimSumControllers = require('./controllers/dimSumController')
 
 app.use(cors());
 app.use(express.json());
@@ -10,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Chinese Dim Sum Spot!');
 });
 
-app.use('/allDimSums', dimSumsControllers);
+app.use('/allDimSums', dimSumControllers);
 app.get('*', (req, res) => {
     res.status(404).send('Not Found');
 });
